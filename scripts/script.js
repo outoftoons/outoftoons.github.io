d639dc53e9c392be1e8c70fcdce698e8ba66a0dc
outoftoons <zjcasas@gmail.com>
2017-02-13 14:48:53 -0500

    reorganizing javascript. Hopefully will work better

--- scripts/script.js
+++ scripts/script.js
@@ -1,115 +1,63 @@
+//what to do on page load
 $(document).ready(function(){
-	$(document).ready(function(){
-		if ($('.content').hasScrollBar()){
-			$('.headingLine:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '17px');
-			});
-			$('.content:not(:contains("zxywvut"))').each(function(){
-				$(this).css('padding-right', '10%');
-			});
-			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '17px');
-			});
-			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-				$(this).css('margin-right', '20px');
-			});
-		}
-		else {
-			$('.headingLine:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '0px');
-			});
-			$('.content:not(:contains("zxywvut"))').each(function(){
-				$(this).css('padding-right', 'calc(10% + 17px)');
-			});
-			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '0px');
-			});
-			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-				$(this).css('margin-right', '3px');
-			});
-		}
-	});
-	$(window).on('resize', function(){
-		if ($('.content').hasScrollBar()){
-			$('.headingLine:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '17px');
-			});
-			$('.content:not(:contains("zxywvut"))').each(function(){
-				$(this).css('padding-right', '10%');
-			});
-			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '17px');
-			});
-			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-				$(this).css('margin-right', '20px');
-			});
-		}
-		else {
-			$('.headingLine:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '0px');
-			});
-			$('.content:not(:contains("zxywvut"))').each(function(){
-				$(this).css('padding-right', 'calc(10% + 17px)');
-			});
-			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-				$(this).css('right', '17px');
-			});
-			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-				$(this).css('margin-right', '3px');
-			});
-		}
-	});
-	
+	//function to determine if there is currently a scrollbar
 	(function($) {
 		$.fn.hasScrollBar = function() {
 			return this.get(0).scrollHeight > this.height();
 		}
 	})(jQuery);
 	
-	$(document).ready(function(){
-		if ($(window).width() < 1081) {
-			$('.headingTable td:has(span)').each(function(){
-				$(this).prev('td').show();
-				$(this).hide();
-				$(this).next('td').hide();
-				$(this).next('td').next('td').show();
-				$(this).next('td').next('td').next('td').hide();
-			});
-		}
-		else {
-			$('.headingTable td:has(span)').each(function(){
-				$(this).prev('td').hide();
-				$(this).show();
-				$(this).next('td').show();
-				$(this).next('td').next('td').hide();
-				$(this).next('td').next('td').next('td').hide();
-			});
-		}
-	});
-	$(window).on('resize', function(){
-		if ($(window).width() < 1081) {
-			$('.headingTable td:has(span)').each(function(){
-				$(this).fadeOut(400, function(){
-					$(this).prev('td').fadeIn(400);//links fade out blank fades in
-				});
-				$(this).next('td').fadeOut(400, function(){
-					$(this).next('td').fadeIn(400);//outlinks fade out hamburger fades in
-				});
-			});
-		}
-		else {
-			$('.hamburgerOverlay').fadeOut(400);
-			$('.headingTable td:has(span)').each(function(){
-				$(this).prev('td').fadeOut(400, function(){
-					$(this).next('td').fadeIn(400);//blank fades out links fade in
-				});
-				$(this).next('td').next('td').fadeOut(400, function(){
-					$(this).prev('td').fadeIn(400);//hamburger fades out outlinks fade in
-				});
-			});
-		}
-	});
-
+	//if there is a scrollbar, change padding and margins so that they don't overlap it and they line up with where they should be with no scrollbar
+	if ($('.content').hasScrollBar()){
+		$('.headingLine:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '17px');
+		});
+		$('.content:not(:contains("zxywvut"))').each(function(){
+			$(this).css('padding-right', '10%');
+		});
+		$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '17px');
+		});
+		$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+			$(this).css('margin-right', '20px');
+		});
+	}
+	else {
+		$('.headingLine:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '0px');
+		});
+		$('.content:not(:contains("zxywvut"))').each(function(){
+			$(this).css('padding-right', 'calc(10% + 17px)');
+		});
+		$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '0px');
+		});
+		$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+			$(this).css('margin-right', '3px');
+		});
+	}
+	
+	//if window is not 1081 pixels wide, change to hamburger layout. if wider than 1080, change to normal layout
+	if ($(window).width() < 1081) {
+		$('.headingTable td:has(span)').each(function(){
+			$(this).prev('td').show();
+			$(this).hide();
+			$(this).next('td').hide();
+			$(this).next('td').next('td').show();
+			$(this).next('td').next('td').next('td').hide();
+		});
+	}
+	else {
+		$('.headingTable td:has(span)').each(function(){
+			$(this).prev('td').hide();
+			$(this).show();
+			$(this).next('td').show();
+			$(this).next('td').next('td').hide();
+			$(this).next('td').next('td').next('td').hide();
+		});
+	}
+	
+	//assign headlink hover colors
 	$(".headingLink:contains('News')").hover(function(){
 		$(this).css('color', 'red');
 	},function(){
@@ -150,6 +98,7 @@ $(document).ready(function(){
 	},function(){
 		$(this).css('color','');
 	});
+	//assign image link hover 
 	$(function() {
 		$(".imageLink")
 			.mouseover(function() { 
@@ -162,6 +111,7 @@ $(document).ready(function(){
 			});
 	});
 	
+	//hamburger hovers
 	var hamburgerColors = [
 		"-red.png",
 		"-orange.png",
@@ -183,6 +133,7 @@ $(document).ready(function(){
 			});
 	});
 	
+	// hamburger overlay visibility
 	$(".hamburger").click(function(){
 		if ( $(".hamburgerOverlay").is(":visible") ) {
 			$('.hamburgerOverlay').fadeOut(400);
@@ -191,7 +142,7 @@ $(document).ready(function(){
 			$('.hamburgerOverlay').fadeIn(400);
 		}
 	});
-	
+	// profile image hovering
 	$(function() {
 		$(".profileImage")
 			.mouseover(function() { 
@@ -203,39 +154,7 @@ $(document).ready(function(){
 				$(this).attr("src", src);
 			});
 	});
-	
-	$(".albumTitle").click(function(){
-		$(this).next('.album').toggle(0,function(){
-			if ($('.content').hasScrollBar()){
-				$('.headingLine:not(:contains("zxywvut"))').each(function(){
-					$(this).css('right', '17px');
-				});
-				$('.content:not(:contains("zxywvut"))').each(function(){
-					$(this).css('padding-right', '10%');
-				});
-				$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-					$(this).css('right', '17px');
-				});
-				$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-					$(this).css('margin-right', '20px');
-				});
-			}
-			else {
-				$('.headingLine:not(:contains("zxywvut"))').each(function(){
-					$(this).css('right', '0px');
-				});
-				$('.content:not(:contains("zxywvut"))').each(function(){
-					$(this).css('padding-right', 'calc(10% + 17px)');
-				});
-				$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
-					$(this).css('right', '17px');
-				});
-				$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
-					$(this).css('margin-right', '3px');
-				});
-			}
-		});
-	});
+	// outlink hover colors
 	var colors = [
 		"red",
 		"orange",
@@ -244,25 +163,122 @@ $(document).ready(function(){
 		"blue",
 		"purple"    
 	];
-	$(document).ready(function(){
-		$('.outLink').each(function(){
-			var colorIndex = Math.floor(Math.random() * colors.length);
-			$(this).css("color", colors[colorIndex])
-		},function(){
-			$(this).css('color','white');
-		});
+	$('.outLink').each(function(){
+		var colorIndex = Math.floor(Math.random() * colors.length);
+		$(this).css("color", colors[colorIndex])
+	},function(){
+		$(this).css('color','white');
 	});
+
 	$(".outLink").hover(function() {
 		$(this).css("color", "white")
 	},function(){
 		var colorIndex = Math.floor(Math.random() * colors.length);
 		$(this).css('color',colors[colorIndex]);
 	});
-
+	
+	// album title hover colors
 	$(".albumTitle").hover(function() {
 		var colorIndex = Math.floor(Math.random() * colors.length);
 		$(this).css("color", colors[colorIndex])
 	},function(){
 		$(this).css('color','white');
+	});
+});
+
+//what to do on window resize
+$(window).on('resize', function(){
+	//function to determine if there is currently a scrollbar
+	(function($) {
+		$.fn.hasScrollBar = function() {
+			return this.get(0).scrollHeight > this.height();
+		}
+	})(jQuery);
+	
+	//if there is a scrollbar, change padding and margins so that they don't overlap it and they line up with where they should be with no scrollbar
+	if ($('.content').hasScrollBar()){
+		$('.headingLine:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '17px');
+		});
+		$('.content:not(:contains("zxywvut"))').each(function(){
+			$(this).css('padding-right', '10%');
+		});
+		$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '17px');
+		});
+		$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+			$(this).css('margin-right', '20px');
+		});
+	}
+	else {
+		$('.headingLine:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '0px');
+		});
+		$('.content:not(:contains("zxywvut"))').each(function(){
+			$(this).css('padding-right', 'calc(10% + 17px)');
+		});
+		$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+			$(this).css('right', '17px');
+		});
+		$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+			$(this).css('margin-right', '3px');
+		});
+	}
+
+	//if window is not 1081 pixels wide, change to hamburger layout. if wider than 1080, change to normal layout
+	if ($(window).width() < 1081) {
+		$('.headingTable td:has(span)').each(function(){
+			$(this).fadeOut(400, function(){
+				$(this).prev('td').fadeIn(400);//links fade out blank fades in
+			});
+			$(this).next('td').fadeOut(400, function(){
+				$(this).next('td').fadeIn(400);//outlinks fade out hamburger fades in
+			});
+		});
+	}
+	else {
+		$('.hamburgerOverlay').fadeOut(400);
+		$('.headingTable td:has(span)').each(function(){
+			$(this).prev('td').fadeOut(400, function(){
+				$(this).next('td').fadeIn(400);//blank fades out links fade in
+			});
+			$(this).next('td').next('td').fadeOut(400, function(){
+				$(this).prev('td').fadeIn(400);//hamburger fades out outlinks fade in
+			});
+		});
+	}
+});
+
+//showing and hiding photos when you click album titles
+$(".albumTitle").click(function(){
+	$(this).next('.album').toggle(0,function(){
+		if ($('.content').hasScrollBar()){
+			$('.headingLine:not(:contains("zxywvut"))').each(function(){
+				$(this).css('right', '17px');
+			});
+			$('.content:not(:contains("zxywvut"))').each(function(){
+				$(this).css('padding-right', '10%');
+			});
+			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+				$(this).css('right', '17px');
+			});
+			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+				$(this).css('margin-right', '20px');
+			});
+		}
+		else {
+			$('.headingLine:not(:contains("zxywvut"))').each(function(){
+				$(this).css('right', '0px');
+			});
+			$('.content:not(:contains("zxywvut"))').each(function(){
+				$(this).css('padding-right', 'calc(10% + 17px)');
+			});
+			$('.hamburgerOverlay:not(:contains("zxywvut"))').each(function(){
+				$(this).css('right', '17px');
+			});
+			$('.imageLinkRight:not(:contains("zxywvut"))').each(function(){
+				$(this).css('margin-right', '3px');
+			});
+		}
 	});
 });
