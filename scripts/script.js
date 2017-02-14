962c5622dbf4e16ce52d49529888621a019e7a0e
outoftoons <zjcasas@gmail.com>
2017-02-14 16:34:45 -0500

    fack

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 		var nPageH = $(document).height();
 		var nViewH = window.outerHeight;
 		if (nViewH > nPageH) {
-			nViewH -= 250;
+			nViewH = nViewH / window.devicePixelRatio;
 			$('BODY').css('height',nViewH + 'px');
 		}
 		window.scrollTo(0,1);
