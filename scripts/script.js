926ec85aa76d9d28030513130628265d01a590f3
outoftoons <zjcasas@gmail.com>
2017-02-14 16:29:46 -0500

    still trying to hide addressbar

--- scripts/script.js
+++ scripts/script.js
@@ -393,7 +393,16 @@ $(document).ready(function(){
 	});
 	
 	//remove addressbar in mobile chrome
-	if(navigator.userAgent.match(/Android/i)){
-		window.scrollTo(0,1);
-	 }
+	$(document).ready(function() {
+		if (navigator.userAgent.match(/Android/i)) {
+			window.scrollTo(0,0); // reset in case prev not scrolled  
+			var nPageH = $(document).height();
+			var nViewH = window.outerHeight;
+			if (nViewH > nPageH) {
+				nViewH -= 250;
+				$('BODY').css('height',nViewH + 'px');
+			}
+			window.scrollTo(0,1);
+		}
+	});
 });
